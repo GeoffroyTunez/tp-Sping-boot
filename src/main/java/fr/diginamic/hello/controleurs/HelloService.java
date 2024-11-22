b31643cd2288640120d2f800e38f583969597c0a
Geoffroy Tunez <145346103+GeoffroyTunez@users.noreply.github.com>
2024-11-22 15:07:41 +0100

    fin tp1

---
+++ src/main/java/fr/diginamic/hello/controleurs/HelloService.java
@@ -0,0 +1,8 @@
+package fr.diginamic.hello.controleurs;
+
+public class HelloService {
+
+    public String salutation(){
+        return "Je suis la classe de service et je vous dis Bonjour";
+    }
+}
